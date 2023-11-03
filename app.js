bd88721007b3d9d8566d6b5adaec7eec0eff5591
siti nurviatika <114561174+nurviatika@users.noreply.github.com>
2023-11-03 20:58:33 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+    const data = getserver("./motoGP.json"); // perbaiki nama file menjadi "motoGP.js"
+    res.json(data);
+});
+
+app.get("/name", (req, res) => {
+    const data = getserver("./name.json");
+    res.json(data);
+});
+
+app.get("/country", (req, res) => {
+    const data = getserver("./country.json"); // file menjadi "name.js"
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log("Server port anda ialah ${port}");
+});
+
+const getserver = (path) => {
+    const data = fs. readFileSync(path, "utf-8");
+    return JSON.parse(data);
+
+}
